4c9481e4e308455cf5b9952e2fba4d653248c9ef
Alex <alexanderrossiter@gmail.com>
2023-01-04 11:25:22 -0500

    Fix weather description text

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,42 @@ console.log('Client side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
+const descriptionP = document.querySelector('#description')
+const currentTempP = document.querySelector('#current-temp')
+const precipitationP = document.querySelector('#precipitation')
+const humidityP = document.querySelector('#humidity')
+const feelslikeP = document.querySelector('#feelslike')
+const locationP = document.querySelector('#location')
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const location = search.value
-
-  messageOne.textContent = 'Loading...'
-  messageTwo.textContent = ''
+  resetText()
+  descriptionP.textContent = 'Loading...'
 
   fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
-        messageOne.textContent = data.error
+        descriptionP.textContent = data.error
       } else {
-        messageOne.textContent = data.forecast
-        messageTwo.textContent = data.location
+        const { temperature, feelslike, weather_descriptions: descriptions, precip, humidity } = data.forecast
+        descriptionP.textContent = descriptions[0] + '.'
+        currentTempP.textContent = 'It is currently ' + temperature + ' degrees.'
+        feelslikeP.textContent = 'It feels like ' + feelslike + ' degrees.'
+        precipitationP.textContent = 'With a ' + precip + '% chance of rain.'
+        humidityP.textContent = humidity + '% humidity.'
+        locationP.textContent = 'In ' + data.location + '.'
       }
     })
   })
 })
+
+const resetText = () => {
+  descriptionP.textContent = ''
+  currentTempP.textContent = ''
+  precipitationP.textContent = ''
+  humidityP.textContent = ''
+  feelslikeP.textContent = ''
+  locationP.textContent = ''
+}
